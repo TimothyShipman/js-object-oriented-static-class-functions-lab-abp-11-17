7d6f4e8f9beb5fe0b5ab923553ff0719b2e5560f
Tim Shipman <bricklayertim@yahoo.com>
2017-11-16 17:43:24 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -2,12 +2,14 @@
 
 class Question{
   constructor(content){
-
+    this.content = content
   }
   static All(){
     return this._All;
   }
-  
+  save(){
+    this.constructor._All.push(this);
+  }
 }
 
 Question._All = []
